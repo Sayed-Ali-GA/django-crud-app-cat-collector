97981873f281e1d3b321a67cad4d6d33dd9dd461
sayed-ali-ga <alsayed3li2005@gmail.com>
2025-08-21 15:53:17 +0300

    update the url

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 
-urlpatterns = [
+urlpatterns = [ 
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-]
+    path('cats/', views.cat_index, name='cat-index'),
+    path('cats/<int:cat_id>/', views.cat_detail, name = "cat-detail"),
+] 
--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
-
-
-# Create your views here.
+from .models import Cat
 
 def home(request):
-    return HttpResponse('<h1>Home Page ᓚᘏᗢ</h1>')
-
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def cat_index(request):
+    cats = Cat.objects.all()
+    return render(request, 'cats/index.html', {'cats': cats})
+
+def cat_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, 'cats/detail.html', {'cat': cat})
