a683bbd8852013bba90fe302eb7607ce6b77475c
sayed-ali-ga <alsayed3li2005@gmail.com>
2025-08-24 19:09:59 +0300

    update

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,16 @@
 from django.db import models
+from django.urls import reverse
+
+
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
+
+
+
+
 
 class Cat (models.Model):
     name = models.CharField(max_length=100)
@@ -8,6 +20,30 @@ class Cat (models.Model):
 
 
 
-    def __str__(self):
+def __str__(self):
       return self.name 
     
+def get_absolute_url(self):
+        return reverse("cat-detail", kwargs={"cat_id": self.id})
+    
+    
+class Feeding(models.Model):
+    date = models.DateField('Feeding Date')
+    meal = models.CharField(
+        max_length=1,
+        choices=MEALS,
+        default=MEALS[0][0]
+    )
+
+  
+
+    # Create a cat_id column for each feeding in the database
+    cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}"
+    
+    class Meta:
+        ordering = ['-date'] 
+
+
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
 from django.http import HttpResponse 
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Cat
+from .forms import FeedingForm
+from django.shortcuts import render, redirect
+
 
 def home(request):
     return render(request, 'home.html')
@@ -14,4 +17,38 @@ def cat_index(request):
 
 def cat_detail(request, cat_id):
     cat = Cat.objects.get(id=cat_id)
-    return render(request, 'cats/detail.html', {'cat': cat})
+    # instantiate FeedingForm to be rendered in the template
+    feeding_form = FeedingForm()
+    return render(request, 'cats/detail.html', {
+        # include the cat and feeding_form in the context
+        'cat': cat, 'feeding_form': feeding_form
+    })
+
+def add_feeding(request, cat_id):
+    # create a ModelForm instance using the data in request.POST
+    form = FeedingForm(request.POST)
+    # validate the form
+    if form.is_valid():
+        # don't save the form to the db until it
+        # has the cat_id assigned
+        new_feeding = form.save(commit=False)
+        new_feeding.cat_id = cat_id
+        new_feeding.save()
+    return redirect('cat-detail', cat_id=cat_id) 
+
+
+
+class CatUpdate(UpdateView):
+    model = Cat
+    # Let's disallow the renaming of a cat by excluding the name field!
+    fields = ['breed', 'description', 'age']
+    success_url = "/cats/"
+
+class CatDelete(DeleteView):
+    model = Cat
+    success_url = '/cats/'
+
+class CatCreate(CreateView):
+    model = Cat
+    fields = '__all__'
+    success_url = "/cats/"
